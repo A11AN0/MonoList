dd9318ef49f6a2d066b53e99294b31d49ee68285
Allan <matrixboston7613@gmail.com>
2023-10-31 18:28:46 +0800

    Initial Commit

---
+++ MonoList/MonoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonoListApp.swift
+//  MonoList
+//
+//  Created by Allan Odunga on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MonoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
