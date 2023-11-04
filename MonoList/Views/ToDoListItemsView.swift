6025fbefef1b4f526b9158845b4dc5eb2b3cfbbf
Allan <matrixboston7613@gmail.com>
2023-11-04 18:02:28 +0800

    renamed content view - and added additional views I might require - adhering to MVVM structure

---
+++ MonoList/Views/ToDoListItemsView.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListItemsView.swift
+//  MonoList
+//
+//  Created by Allan Odunga on 4/11/2023.
+//
+
+import SwiftUI
+
+struct ToDoListItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToDoListItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListItemsView()
+    }
+}
