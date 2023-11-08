69e9a671e01fe325c90fa91e33e21aea2c36590e
Allan <matrixboston7613@gmail.com>
2023-11-08 22:03:53 +0800

    General Register view layout

--- MonoList/Views/RegisterView.swift
+++ MonoList/Views/RegisterView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             //Header
             HeaderView(title: "Register", subtitle: "Create your account", angle: -15, background: .blue)
             
-            Spacer()
+            //Form
+            Form {
+                TextField("Full Name", text:$name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                TextField("Email Address", text:$email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                SecureField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                Button {
+                    //action to login goes here
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                        Text("Register")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                    }
+                    
+                }
+                .padding()
+            }
+            .scrollContentBackground(.hidden)
+            .offset(y: -50)
         }
     }
 }
