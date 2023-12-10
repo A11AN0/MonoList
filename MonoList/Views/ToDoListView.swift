f98f2980ff7061de6bf4d766e7357ca340e33a8e
Allan <matrixboston7613@gmail.com>
2023-12-10 21:14:49 +0800

    toDoList item views created for item display on toDoList - and then slideAction delete incorporated for individual items

--- MonoList/Views/ToDoListView.swift
+++ MonoList/Views/ToDoListView.swift
@@ -5,22 +5,39 @@
 //  Created by Allan Odunga on 4/11/2023.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
     @StateObject var viewModel = ToDoListViewViewModel()
+    @FirestoreQuery var items: [ToDoListItem]
     
     private let userId: String
     
     
     init(userId: String) {
         self.userId = userId
+        
+        //Collection path for the toDoList entries would be - users/<id>/todos/<entries>
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) {item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button {
+                                viewModel.delete(id: item.id)
+                            } label: {
+                                    Text("Delete")
+                            }
+                        }
+                        .tint(.red)
+                    
+                }
+                .listStyle(PlainListStyle())
             }
             .navigationTitle("To Do List")
             .toolbar {
@@ -39,6 +56,6 @@ struct ToDoListView: View {
 
 struct ToDoListView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListView(userId: "")
+        ToDoListView(userId: "ExampleId")
     }
 }
