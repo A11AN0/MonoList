f40ab7a459bd18b65ec99b5d6951b88e17bb8e7f
Allan <matrixboston7613@gmail.com>
2023-12-20 18:16:41 +0800

    logout functionality - and user profile details in profile view

--- MonoList/ViewModels/ToDoListItemViewViewModel.swift
+++ MonoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -6,9 +6,30 @@
 //
 
 import Foundation
-
+import FirebaseAuth
+import FirebaseFirestore
 
 //ViewModel for a single todo-list item view (single row entry in toDoList)
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        //toggle itemIsDone property
+        var itemDuplicate = item
+        itemDuplicate.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemDuplicate.id)
+            .setData(itemDuplicate.asDictionary())
+    }
+    
 }
+
--- MonoList/Views/ProfileView.swift
+++ MonoList/Views/ProfileView.swift
@@ -14,8 +14,57 @@ struct ProfileView: View {
         NavigationView {
             VStack {
                 
+                if let user = viewModel.user {
+                    userProfile(user: user)
+                    //Sign out
+                    TLButton(title: "Log out", backgroundColor: .red) {
+                        viewModel.logOut()
+                    }
+                    .frame(width: 200, height: 45)
+                } else {
+                    Text("Loading profile...")
+                }
+                
+                Spacer()
+                
             }
             .navigationTitle("Profile")
+        }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+}
+
+@ViewBuilder
+func userProfile(user: User) -> some View {
+    //Avatar
+    Image(systemName: "person.circle")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .foregroundColor(.blue)
+        .frame(width: 125, height: 125)
+    
+    //Info: Name, email, member since
+    VStack(alignment: .leading) {
+        HStack {
+            Text("Name: ")
+                .fontWeight(.bold)
+                .padding()
+            
+            Text(user.name)
+        }
+        HStack {
+            Text("Email:")
+                .fontWeight(.bold)
+                .padding()
+            Text(user.email)
+        }
+        HStack {
+            Text("Member Since:")
+                .fontWeight(.bold)
+                .padding()
+            Text("\( Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: . shortened))")
         }
     }
 }
